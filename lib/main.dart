9ae3f7addaa0735ac5c953aedf738dd9980f0621
devmotive <ahmad.codespace@gmail.com>
2025-08-05 13:25:50 +0700

    feat: Add Google Fonts package and apply Poppins theme
    - Added `google_fonts` dependency.
    - Updated `MaterialApp` to use `ThemeData` with `GoogleFonts.poppinsTextTheme()`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+    );
   }
 }
