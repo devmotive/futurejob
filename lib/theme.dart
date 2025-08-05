0fa15dd3de7f5d44a42d511fb7c2abf2bdb416ef
devmotive <ahmad.codespace@gmail.com>
2025-08-05 13:26:32 +0700

    feat: Add color palette

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+// Color Palette
+Color kIndigoColor = Color(0xff4141A4);
+Color kDarkIndigoColor = Color(0xff2A327D);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kGreyColor = Color(0xffB3B5C4);
+Color kLightGreyColor = Color(0xffF1F0F5);
+Color kBlackColor = Color(0xff272C2F);
+Color kRedColor = Color(0xffFD4F56);
