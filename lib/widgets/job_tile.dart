0f956f3145450a6aa80fd585d213ccda5d8c0fe2
devmotive <ahmad.codespace@gmail.com>
2025-08-06 11:53:00 +0700

    feat: Add JobTile widget
    - Displays a job listing with an image, job title, and company name.
    - Uses `ListTile` for layout.
    - Image is loaded using `Image.asset`.
    - Text styling is applied using custom theme colors and font weights.
    - TODO: Added a comment to handle image loading errors.

---
+++ lib/widgets/job_tile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:futurejob/theme.dart';
+
+class JobTile extends StatelessWidget {
+  const JobTile({
+    super.key,
+    required this.imageUrl,
+    required this.jobTitle,
+    required this.companyName,
+  });
+
+  // The URL of the image to display for the job.
+  final String imageUrl;
+  // The title of the job.
+  final String jobTitle;
+  // The name of the company offering the job.
+  final String companyName;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      // Leading widget is displayed at the beginning of the ListTile.
+      // TODO: Add error handling for image loading (e.g., placeholder or error widget).
+    leading: Image.asset(imageUrl, height: 45, fit: BoxFit.cover),
+      // Title is the primary content of the ListTile.
+      title: Text(
+        jobTitle,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: medium,
+          color: kBlackColor,
+        ),
+      ),
+      // Subtitle is displayed below the title.
+      subtitle: Text(
+        companyName,
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: regular,
+          color: kGreyColor,
+        ),
+      ),
+    );
+  }
+}
