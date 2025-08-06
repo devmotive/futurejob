94932bbd9efda6ed4ef39594030ffffda11bb6d4
devmotive <ahmad.codespace@gmail.com>
2025-08-06 13:19:21 +0700

    refactor: Remove redundant whitespace in `job_tile.dart`

--- lib/widgets/job_tile.dart
+++ lib/widgets/job_tile.dart
@@ -11,34 +11,29 @@ class JobTile extends StatelessWidget {
 
   // The URL of the image to display for the job.
   final String imageUrl;
+
   // The title of the job.
   final String jobTitle;
+
   // The name of the company offering the job.
   final String companyName;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      contentPadding: EdgeInsets.zero,
       // Leading widget is displayed at the beginning of the ListTile.
       // TODO: Add error handling for image loading (e.g., placeholder or error widget).
-    leading: Image.asset(imageUrl, height: 45, fit: BoxFit.cover),
+      leading: Image.asset(imageUrl, height: 45, fit: BoxFit.cover),
       // Title is the primary content of the ListTile.
       title: Text(
         jobTitle,
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: medium,
-          color: kBlackColor,
-        ),
+        style: TextStyle(fontSize: 16, fontWeight: medium, color: kBlackColor),
       ),
       // Subtitle is displayed below the title.
       subtitle: Text(
         companyName,
-        style: TextStyle(
-          fontSize: 14,
-          fontWeight: regular,
-          color: kGreyColor,
-        ),
+        style: TextStyle(fontSize: 14, fontWeight: regular, color: kGreyColor),
       ),
     );
   }
